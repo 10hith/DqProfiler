07dbc637944bb19150b2ed2171b07ad16ad4a49c
Lohith Basavaraj <lohith.uvce@gmail.com>
2021-09-03 14:48:26 +0100

    initial commit

---
+++ src/main/scala/sampleProfie.scala
@@ -0,0 +1,32 @@
+import ProfileHelpers.runProfile
+import org.apache.spark.sql.SparkSession
+
+object sampleProfie {
+  def main(args: Array[String]): Unit = {
+
+    val spark = {
+      SparkSession.builder()
+        .appName("deUtils")
+        .master("local")
+        //      .enableHiveSupport()
+        .getOrCreate()
+    }
+
+    val currentDirectory = new java.io.File(".").getCanonicalPath
+    val ipDF = spark.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .load(s"$currentDirectory/src/test/resources/testDataSet_1.csv")
+
+
+    spark.conf.getAll map (println(_))
+
+//    ipDF.show(200)
+    ipDF.printSchema()
+
+    val profiledDF = runProfile(ipDF)
+
+    profiledDF.show(100)
+
+  }
+}
